3dd069eb491ea4986022522e60dd0a5407396a5e
Mateus <mateus.gotardi@gmail.com>
2022-06-05 15:29:44 -0300

    jogo da velha

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GameProvider } from './providers/game';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GameProvider>
+      <App />
+    </GameProvider>
   </React.StrictMode>
 );
 
